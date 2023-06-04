7d59c32c5731b6edf1b5824138faa5c206cbee40
vasil <vasil@LAPTOP-0D53CULI>
2023-06-04 20:50:28 +0300

    add seminar app and models

--- calendar1/settings.py
+++ calendar1/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'course',
     'event',
     'professor',
-    'account'
+    'account',
+    'seminar'
 ]
 
 MIDDLEWARE = [
---
+++ seminar/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Seminar(models.Model):
+    title = models.CharField(max_length=500)
+    is_online = models.BooleanField(default=False)
+    url = models.CharField(max_length=1000, null=True)
+    speaker_fullname = models.CharField(max_length=500)
+    location = models.CharField(max_length=1000, null=True)
+
+class CalendarSeminar(models.Model):
+    seminar = models.ForeignKey(Seminar, on_delete=models.DO_NOTHING)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
